0f34ef90dfb803d52bcc5b08b2c4d8c6eeb985c9
RabinAd <45183591+RabinAd@users.noreply.github.com>
2021-02-26 16:28:15 -0600

    Add files via upload

---
+++ WaitList.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Waitlist implements Serializable {
+  private static final long serialVersionUID = 1L;
+  private Product product;
+  private Client client;
+  private int quantity;
+
+  public Waitlist(Client client, Product product, int quantity){
+    this.client   = client;
+    this.product  = product;
+    this.quantity = quantity;
+  }
+
+  public Product getProduct(){
+    return product;
+  }
+
+  public Client getClient(){
+    return client;
+  }
+
+  public int getQuantity(){
+    return quantity;
+  }
+
+  public void updateQuantity(int newQuantity){
+    this.quantity = newQuantity;
+  }
+}
